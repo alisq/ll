23cf10f0d1b26ee87bcc1d3f5fe51e2af3dbeeba
alisq <ali.shamas.qadeer@gmail.com>
2021-11-29 22:19:24 -0500

    added reflection

--- script.js
+++ script.js
@@ -1,19 +1,48 @@
+let w=$(window).width();
+let eW = $(".floor").width()*2-40;
+console.log(w/eW)
+
+$("#everything").css({
+    "transform":"scale("+w/eW+")"
+})
+
+
+$("#everything").css({
+
+})
+
 $(".column").each(function(){
-    for (i=0;i<100;i++) {
+    for (i=0;i<15;i++) {
         $("<div class='tile'></div>")
             .appendTo($(this))
     }
 })
 
+dupeFloor();
+
 $(document).on("mouseover",".tile",function(e){
     if(e.buttons == 1 || e.buttons == 3){
         //do some stuff
         $(this).toggleClass("white")
+        dupeFloor()
     }        
 })
 
 
 $(document).on("click",".tile",function(e){
-        $(this).toggleClass("white")
+    
+        $(this).toggleClass("white");
+        dupeFloor()
       
 })
+
+
+function dupeFloor() {
+    var c = $("#floor1").html();
+    // console.log(c)
+    $("#floor2").html(c)
+    $("#floor3").html(c)
+    $("#floor4").html(c)
+
+
+}
