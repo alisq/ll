505cccc1588e9749466ba7987d8c62cbc3ad06c9
alisq <ali.shamas.qadeer@gmail.com>
2021-11-29 21:30:04 -0500

    complete change

--- script.js
+++ script.js
@@ -1,46 +1,19 @@
+$(".column").each(function(){
+    for (i=0;i<100;i++) {
+        $("<div class='tile'></div>")
+            .appendTo($(this))
+    }
+})
 
-//BELOW: code for the hover state that reveals the images
-
-$(".posterpicshover").hover(function(details){ // adding an event listener to poster pics hover + recording event information in a variable called "details" (which we just created)
-
-	console.log(details.clientY) //inside this details variable is the information about where this mouse is, and the console is spitting it out
-
-	$(".posterpic")
-		.css({
-			"top":details.clientY
-		}) //
-
- $(".posterpic")
-        .css({
-            "top": 30, "left": 30, "position":'absolute'});
-
-}, function() { //when you are no longer hovering.
-	$(".posterpic").attr("style","") //clear out the "style" attribute by filling it with nothing + it then goes m=back to the css style / placement
+$(document).on("mouseover",".tile",function(e){
+    if(e.buttons == 1 || e.buttons == 3){
+        //do some stuff
+        $(this).toggleClass("white")
+    }        
 })
 
 
-
-// BELOW: Code for the scroll to top
-
-
-$("#top").on("click", function() {
-    $(".box.one").scrollTop(0);
-});
-
-
-
-//BELOW: code for the slideshow
-
-$(".posterpic > div:gt(0)").hide();
-
-setInterval(function() { 
-  $('.posterpic > div:first')
-    .fadeOut(0)
-    .next(1000)
-    .fadeIn(0)
-    .end()
-    .appendTo('.posterpic');
-},  1200);
-
-
-
+$(document).on("click",".tile",function(e){
+        $(this).toggleClass("white")
+      
+})
